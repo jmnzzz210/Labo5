23f38a63827fb954a48366ce5d5058a81a885a83
Brandon <brandon.jimenezcampos@ucr.ac.cr>
2025-06-14 18:50:18 -0600

    Arreglo del dynamic_array.h, se colocó datos, en vez de dato en el struct y se arregló el nombre remove, ya que tiraba un warning de que se podría confundir con otra funcionalidad

--- include/dynamic_array.h
+++ include/dynamic_array.h
@@ -5,14 +5,14 @@
 #include <stddef.h>
 
 typedef struct {
-    int* dato;
+    int* datos;
     size_t tamano;
     size_t tamano_inicial;
 } arreglod;
 
 arreglod* create(size_t tamano_inicial); // función que crea el arreglo
 void append(arreglod* arr, int numero);  // función que agrega un elemento al final del arreglo
-void remove(arreglod* arr, int numero); //función que elimina un elemento del arreglo
+void remove_element(arreglod* arr, int numero); //función que elimina un elemento del arreglo
 int get(arreglod* arr, size_t ubicacion); //función para obtener elemento por su índice
 void free_arreglod(arreglod* arr); // función que libera la memoria del puntero
 void print(const arreglod* arr); // función que imprime el arreglo
